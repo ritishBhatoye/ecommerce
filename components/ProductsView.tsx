8066b435a0c1ecf326ecdcccb12acba2ebbdb7a7
Ritish Bhatoye <ritishrahul87@gmail.com>
2024-12-15 18:45:28 +0530

    CATEGORIES INIT

--- components/ProductsView.tsx
+++ components/ProductsView.tsx
@@ -1,5 +1,6 @@
 import { Category, Product } from "@/sanity.types";
 import ProductGrid from "./ProductGrid";
+import CategorySelectorComponent from "./CategorySelectorComponent";
 
 
 
@@ -12,7 +13,7 @@ export const ProductsView=({products,categories}:ProductsViewProps)=>{
     return <div className="flex flex-col ">
         {/* categories */}
         <div className="w-full sm:w=[200px]">
-
+        <CategorySelectorComponent categories={categories}/>
         </div>
         {/* Products */}
 
